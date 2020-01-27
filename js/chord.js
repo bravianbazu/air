fbb6a945221a3a6bac58bdeb34a0e9e3863e173d
msimbao <32774483+msimbao@users.noreply.github.com>
2020-01-27 15:18:29 -0600

    Update chord.js

--- js/chord.js
+++ js/chord.js
@@ -2,31 +2,31 @@ function getchord() {
   // alert('hi');
   //amajor
   if ( (E == 1) && (A == 1) && (C_ == 1) ){
-      audio.src = 'https://cdn.glitch.com/bde82fbb-ee14-4c97-a669-94a07bcbedb3%2Famajor.wav?v=1580127638637';
+      audio.src = '../chords/amajor.wav';
       }
       
   //cmajor
   else if ((C == 1) && (E == 1) && (Ca == 1)){
-      audio.src = 'https://cdn.glitch.com/bde82fbb-ee14-4c97-a669-94a07bcbedb3%2Fcmajor.wav?v=1580127634437';
+      audio.src = '../chords/cmajor.wav';
       }
   //dmajor
   else if ((f_ == 1) && (D == 1) && (A == 1)){
-      audio.src = 'https://cdn.glitch.com/bde82fbb-ee14-4c97-a669-94a07bcbedb3%2Fdmajor.wav?v=1580127634501'
+      audio.src = '../chords/dmajor.wav'
     
   }
   //emajor
   else if ((G_ == 1) && (E == 1) && (B_ == 1)){
-      audio.src = 'https://cdn.glitch.com/bde82fbb-ee14-4c97-a669-94a07bcbedb3%2Femajor.wav?v=1580127634332'
+      audio.src = '../chords/emajor.wav'
     
   }
   //fmajor
   else if ((f == 1) && (C == 1) && (A == 1) && (Fd == 1)){
-      audio.src = 'https://cdn.glitch.com/bde82fbb-ee14-4c97-a669-94a07bcbedb3%2Ffmajor.wav?v=1580127634627'
+      audio.src = '../chords/fmajor.wav'
     
   }
   //gmajor
   else if ((B == 1) && (G == 1) && (g == 1)){
-      audio.src = 'https://cdn.glitch.com/bde82fbb-ee14-4c97-a669-94a07bcbedb3%2Fgmajor.wav?v=1580127634513'
+      audio.src = '../chords/gmajor.wav'
    
   }
 }
