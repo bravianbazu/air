5720ae747b8f3a50cd19842d5fb2cc40177b1de9
msimbao <32774483+msimbao@users.noreply.github.com>
2020-01-27 12:02:28 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ function capture(payload) {
 	
   //Audio controls
 	
-  if (payload.score > 450) {
+  if (payload.score > 400) {
     audio.pause();
     audio.currentTime = 0;
     payload.score = 0;    
