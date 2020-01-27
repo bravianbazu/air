644caeb9047a6c32977eca33d74f4f47cb8f1e6b
msimbao <32774483+msimbao@users.noreply.github.com>
2020-01-26 22:15:27 -0600

    Update chords.js

--- js/chords.js
+++ js/chords.js
@@ -5,5 +5,6 @@ function getchord() {
   
   if ((E == 1) & (A == 1) & (C_ == 1)){
       audio.src = 'https://raw.githubusercontent.com/scribbletune/sampler/master/samples/korgBass/a1-33.wav'
+    alert("src changed)
       }
 }
