c9ea610de3c309e7c89c0ce8877c0665ea5215c8
msimbao <32774483+msimbao@users.noreply.github.com>
2020-01-27 11:36:49 -0600

    Update fretboard.js

--- js/fretboard.js
+++ js/fretboard.js
@@ -24,39 +24,6 @@ var f = 0;
 var f_ = 0;
 var g = 0;
 
-function reset() {
-  
-    if ((E == 1) && (A == 1) && (C_ == 1)){
-      audio.src = 'https://cdn.glitch.com/bde82fbb-ee14-4c97-a669-94a07bcbedb3%2Famajor.wav?v=1580127638637'
-      }
-  //bmajor
-  // else if ((E == 1) && (A == 1) && (C_ == 1)){
-  //     audio.src = 'https://cdn.glitch.com/bde82fbb-ee14-4c97-a669-94a07bcbedb3%2Famajor.wav?v=1580127638637'
-  //     }
-  //cmajor
-  else if ((C == 1) && (E == 1) && (Ca == 1)){
-      audio.src = 'https://cdn.glitch.com/bde82fbb-ee14-4c97-a669-94a07bcbedb3%2Fcmajor.wav?v=1580127634437'
-      }
-  //dmajor
-  else if ((f_ == 1) && (D == 1) && (A == 1)){
-      audio.src = 'https://cdn.glitch.com/bde82fbb-ee14-4c97-a669-94a07bcbedb3%2Fdmajor.wav?v=1580127634501'
-      }
-  //emajor
-  else if ((G_ == 1) && (E == 1) && (B_ == 1)){
-      audio.src = 'https://cdn.glitch.com/bde82fbb-ee14-4c97-a669-94a07bcbedb3%2Femajor.wav?v=1580127634332'
-      }
-  //fmajor
-  else if ((f == 1) && (C == 1) && (A == 1) && (Fd == 1)){
-      audio.src = 'https://cdn.glitch.com/bde82fbb-ee14-4c97-a669-94a07bcbedb3%2Ffmajor.wav?v=1580127634627'
-      }
-  //gmajor
-  else if ((B == 1) && (G == 1) && (g == 1)){
-      audio.src = 'https://cdn.glitch.com/bde82fbb-ee14-4c97-a669-94a07bcbedb3%2Fgmajor.wav?v=1580127634513'
-  }
-}
-
-setInterval(reset, 500);
-
 // Log events flag
 var logEvents = false;
 
