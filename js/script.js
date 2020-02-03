279c8f457cdbab436827af8a0f688072946622e0
msimbao <32774483+msimbao@users.noreply.github.com>
2020-02-02 21:35:35 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ setInterval(getchord, 50);
 function capture(payload) {
 	score.textContent = payload.score;
   
-  if (payload.score > 120) {
+  if (payload.score > 170) {
     audio.pause();
     audio.currentTime = 0;
     console.log("lift off!!");
